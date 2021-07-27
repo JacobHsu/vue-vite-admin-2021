76846fc7a999df55779844aa2748aadd9ef5f7f4
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-27 15:00:27 +0800

    feat: App.vue ConfigProvider ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <RouterView />
+  <ConfigProvider>
+    <RouterView />
+  </ConfigProvider>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-
+  import { ConfigProvider } from 'ant-design-vue';
   export default defineComponent({
-    name: 'App'
+    name: 'App',
+    components: { ConfigProvider },
   });
 </script>
-
-
