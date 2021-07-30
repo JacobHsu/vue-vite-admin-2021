5eca6aa3792edf9d309fad589b004a06a8c47c6a
jacobhsu <jacob.hsu.tw@gmail.com>
2021-07-30 11:41:49 +0800

    feat: App.vue useTitle @vueuse/core

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <ConfigProvider>
+  <ConfigProvider :locale="getAntdLocale">
     <RouterView />
   </ConfigProvider>
 </template>
@@ -8,8 +8,19 @@
 <script lang="ts">
   import { defineComponent } from 'vue';
   import { ConfigProvider } from 'ant-design-vue';
+  import { useTitle } from '/@/hooks/web/useTitle';
+  import { useLocale } from '/@/locales/useLocale';
+
   export default defineComponent({
     name: 'App',
     components: { ConfigProvider },
+    setup() {
+      useTitle();
+
+      // support Multi-language
+      const { getAntdLocale } = useLocale();
+
+      return { getAntdLocale };
+    },
   });
 </script>
