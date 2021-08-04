ab046073e2fa16996905cea72ba49831c8bd49bf
jacobhsu <jacob.hsu.tw@gmail.com>
2021-08-04 17:22:11 +0800

    feat: routes EXCEPTION_COMPONENT

--- src/router/routes/basic.ts
+++ src/router/routes/basic.ts
@@ -3,7 +3,7 @@ import { t } from '/@/hooks/web/useI18n';
 import {
   REDIRECT_NAME,
   LAYOUT,
-  // EXCEPTION_COMPONENT,
+  EXCEPTION_COMPONENT,
   PAGE_NOT_FOUND_NAME,
 } from '/@/router/constant';
 
@@ -17,41 +17,41 @@ export const PAGE_NOT_FOUND_ROUTE: AppRouteRecordRaw = {
     hideBreadcrumb: true,
     hideMenu: true,
   },
-  // children: [
-  //   {
-  //     path: '/:path(.*)*',
-  //     name: PAGE_NOT_FOUND_NAME,
-  //     component: EXCEPTION_COMPONENT,
-  //     meta: {
-  //       title: 'ErrorPage',
-  //       hideBreadcrumb: true,
-  //       hideMenu: true,
-  //     },
-  //   },
-  // ],
+  children: [
+    {
+      path: '/:path(.*)*',
+      name: PAGE_NOT_FOUND_NAME,
+      component: EXCEPTION_COMPONENT,
+      meta: {
+        title: 'ErrorPage',
+        hideBreadcrumb: true,
+        hideMenu: true,
+      },
+    },
+  ],
 };
 
-// export const REDIRECT_ROUTE: AppRouteRecordRaw = {
-//   path: '/redirect',
-//   name: REDIRECT_NAME,
-//   component: LAYOUT,
-//   meta: {
-//     title: REDIRECT_NAME,
-//     hideBreadcrumb: true,
-//     hideMenu: true,
-//   },
-//   children: [
-//     {
-//       path: '/redirect/:path(.*)',
-//       name: REDIRECT_NAME,
-//       component: () => import('/@/views/sys/redirect/index.vue'),
-//       meta: {
-//         title: REDIRECT_NAME,
-//         hideBreadcrumb: true,
-//       },
-//     },
-//   ],
-// };
+export const REDIRECT_ROUTE: AppRouteRecordRaw = {
+  path: '/redirect',
+  name: REDIRECT_NAME,
+  component: LAYOUT,
+  meta: {
+    title: REDIRECT_NAME,
+    hideBreadcrumb: true,
+    hideMenu: true,
+  },
+  children: [
+    {
+      path: '/redirect/:path(.*)',
+      name: REDIRECT_NAME,
+      component: () => import('/@/views/sys/redirect/index.vue'),
+      meta: {
+        title: REDIRECT_NAME,
+        hideBreadcrumb: true,
+      },
+    },
+  ],
+};
 
 export const ERROR_LOG_ROUTE: AppRouteRecordRaw = {
   path: '/error-log',
