dd701e0588e61e0019b8f6a77d56730f9c3a448b
jacobhsu <jacob.hsu.tw@gmail.com>
2021-08-24 17:58:39 +0800

    feat: view login

--- src/views/sys/login/Login.vue
+++ src/views/sys/login/Login.vue
@@ -7,11 +7,11 @@
     />
    <AppDarkModeToggle class="absolute top-3 right-7 enter-x" v-if="!sessionTimeout" />
 
-     <!-- <span class="-enter-x xl:hidden">
+     <span class="-enter-x xl:hidden">
       <AppLogo :alwaysShowTitle="true" />
-    </span> -->
+    </span>
 
-    <!-- <div class="container relative h-full py-2 mx-auto sm:px-10">
+    <div class="container relative h-full py-2 mx-auto sm:px-10">
       <div class="flex h-full">
         <div class="hidden min-h-full pl-4 mr-4 xl:flex xl:flex-col xl:w-6/12">
           <AppLogo class="-enter-x" />
@@ -51,14 +51,14 @@
             "
           >
             <LoginForm />
-            <ForgetPasswordForm />
+            <!-- <ForgetPasswordForm />
             <RegisterForm />
             <MobileForm />
-            <QrCodeForm />
+            <QrCodeForm /> -->
           </div>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 <script lang="ts">
