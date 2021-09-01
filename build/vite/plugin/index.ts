535bba76bfc241467665e7a49c348b7319b7abad
jacobhsu <jacob.hsu.tw@gmail.com>
2021-09-01 17:45:34 +0800

    feat: npm vite-plugin-compression

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -7,6 +7,7 @@ import purgeIcons from 'vite-plugin-purge-icons';
 
 import { configHtmlPlugin } from './html';
 import { configMockPlugin } from './mock';
+import { configCompressPlugin } from './compress';
 import { configStyleImportPlugin } from './styleImport';
 import { configThemePlugin } from './theme';
 import { configImageminPlugin } from './imagemin';
@@ -51,6 +52,11 @@ export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
 
   // The following plugins only work in the production environment
   if (isBuild) {
+    // rollup-plugin-gzip
+    vitePlugins.push(
+      configCompressPlugin(VITE_BUILD_COMPRESS, VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE)
+    );
+
     //vite-plugin-imagemin
     VITE_USE_IMAGEMIN && vitePlugins.push(configImageminPlugin());
   }
